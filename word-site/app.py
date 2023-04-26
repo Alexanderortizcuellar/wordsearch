7a46615b4d0feb114540244f316794fa0d2b1e9a
alexander otiz <alexander2magnus@gmail.com>
2023-04-26 16:07:58 -0500

    commit

--- word-site/app.py
+++ word-site/app.py
@@ -2,22 +2,27 @@ import string
 from random import choice
 
 import tabulate
-from flask import Flask, redirect, render_template
-
+from flask import Flask, redirect, render_template, request,url_for
+from ..wordsearch.wordsearch import WordSearch
 app = Flask(__name__)
 
 
-grid = [[choice(string.ascii_uppercase) for col in range(20)]
-        for row in range(20)]
-grid = tabulate.tabulate(grid, tablefmt="html")
-
-
-@app.route("/")
+@app.route("/", methods=["post", "get"])
 def home():
-    return render_template("index.html", grid=grid)
+    width = request.args.get("width")
+    height = request.args.get("height")
+    show_answers = request.args.get("answers")
+    if height is None or width is None:
+        width,height = 10,10 
+    grid = [[choice(string.ascii_uppercase) for col in range(int(width))]
+            for row in range(int(height))]
+    grid = tabulate.tabulate(grid, tablefmt="html")
+    return render_template("index.html", grid=grid, width=width, height=height)
+
+
+
 
 
 if __name__ == "__main__":
     app.run("localhost", 8080, debug=True)
-
 
